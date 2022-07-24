6875eeb56786bb2e5b96126020c8844404e47054
mardulius <kotel77@inbox.ru>
2022-07-24 14:43:09 +0300

    Add project files.

---
+++ Consumer.Error/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+var connection = factory.CreateConnection();
+
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare("directs_logs", ExchangeType.Direct);
+    var queueName = channel.QueueDeclare().QueueName;
+
+    channel.QueueBind(queueName, "directs_logs", "error");   
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (sender, args) =>
+    {
+        var body = args.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Console.WriteLine($"Получено сообщение:{message}");
+    };
+    channel.BasicConsume(queueName, true, consumer);
+    Console.WriteLine($"подписались на очередь {queueName}");
+    Console.ReadLine();
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+Task.Run(CreateTask(12000, "error"));
+Task.Run(CreateTask(8000, "info"));
+Task.Run(CreateTask(15000, "warning"));
+
+
+Console.ReadLine();
+
+
+
+static Func<Task> CreateTask(int timeToSleepTo, string routingKey)
+{
+    return () =>
+    {
+
+        var counter = 0;
+
+        do
+        {
+            int timeToSleep = new Random().Next(1000, timeToSleepTo);
+            Thread.Sleep(timeToSleep);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            var connection = factory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("directs_logs", ExchangeType.Direct);
+                string message = $"здороува мужики,{routingKey} произошла ошибка! сообщение[{counter}]";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "directs_logs",
+                    routingKey: routingKey,
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($"Сообщение [{counter++}] типа:{routingKey} - отправлено");
+            }
+        }
+        while (true);
+
+    };
+
+    
+}
